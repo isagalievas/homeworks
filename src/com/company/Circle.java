1f75fabc696fb1b0298c1c22ce6dc779bdcb6949
Sezim <isagalievasezimka@gmail.com>
2021-12-25 21:16:58 +0600

    1

---
+++ src/com/company/Circle.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+
+    }
+}
