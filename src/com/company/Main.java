617042d9847f201cab3e267ef3dc82fcd6c63fe1
Sezim <isagalievasezimka@gmail.com>
2022-02-26 14:16:44 +0600

    examination4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println();
+        Set<Character> characters = new HashSet<>();
+        characters.add('1');
+        characters.add('2');
+        characters.add('3');
+        characters.add('4');
+        characters.add('4');
+        characters.add('5');
+        System.out.println(characters);
     }
 }
