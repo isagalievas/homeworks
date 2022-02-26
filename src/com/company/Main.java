5a8d4ba6ce2394ffdfd79f40942ddbccd82da0d8
Sezim <isagalievasezimka@gmail.com>
2022-02-26 17:21:07 +0600

    examination3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println();
+        List<Pet> pets = new ArrayList<>();
+
     }
 }
