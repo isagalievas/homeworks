57bb701d03ece46af43c8242d3cf4ff71f532d96
Sezim <isagalievasezimka@gmail.com>
2022-02-26 21:51:52 +0600

    examination4.1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,7 @@ import java.util.Set;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Введите число (НАПРИМЕР 1 2 3 4 4 5): ");
+        System.out.println("Введите число (ПРИМЕР 1 2 3 4 4 5 ...): ");
         String numbers = new Scanner(System.in).nextLine();
         Set<String> set = new HashSet<>();
         set.addAll(Arrays.asList(numbers.split(" ")));
