bf2a6d1401d94ad0e9bebc93aadf651f9ebc1677
Sezim <isagalievasezimka@gmail.com>
2022-02-26 20:52:37 +0600

    examination4.4

--- src/com/company/Group.java
+++ src/com/company/Group.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Group {
     String groupName;
@@ -48,14 +49,25 @@ public class Group {
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (obj == this) {
-            return true;
-        }
-        if (obj == null || obj.getClass() != this.getClass()) {
-            return false;
-        }
-        Group group = (Group) obj;
-        return numberOfGroup == group.numberOfGroup && groupName == group.groupName ||
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Group group = (Group) o;
+        return numberOfGroup == group.numberOfGroup && groupName.equals(group.groupName) && course.equals(group.course) && students.equals(group.students);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(groupName, course, numberOfGroup, students);
+    }
+
+    @Override
+    public String toString() {
+        return "Group {" +
+                "groupName ='" + groupName + '\'' +
+                ", course ='" + course + '\'' +
+                ", numberOfGroup = " + numberOfGroup +
+                ", students = " + students +
+                '}';
     }
 }
--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -1,37 +1,61 @@
 package com.company;
 
+import java.util.Objects;
+
 public class Student {
-   int id;
-   String name;
-   int gradesBySubject;
+    int id;
+    String name;
+    int gradesBySubject;
 
-   public Student(int id, String name, int gradesBySubject) {
-      this.id = id;
-      this.name = name;
-      this.gradesBySubject = gradesBySubject;
-   }
+    public Student(int id, String name, int gradesBySubject) {
+        this.id = id;
+        this.name = name;
+        this.gradesBySubject = gradesBySubject;
+    }
 
-   public int getId() {
-      return id;
-   }
+    public int getId() {
+        return id;
+    }
 
-   public void setId(int id) {
-      this.id = id;
-   }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-   public String getName() {
-      return name;
-   }
+    public String getName() {
+        return name;
+    }
 
-   public void setName(String name) {
-      this.name = name;
-   }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-   public int getGradesBySubject() {
-      return gradesBySubject;
-   }
+    public int getGradesBySubject() {
+        return gradesBySubject;
+    }
 
-   public void setGradesBySubject(int gradesBySubject) {
-      this.gradesBySubject = gradesBySubject;
-   }
+    public void setGradesBySubject(int gradesBySubject) {
+        this.gradesBySubject = gradesBySubject;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id && gradesBySubject == student.gradesBySubject && name.equals(student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, gradesBySubject);
+    }
+
+    @Override
+    public String toString() {
+        return "Student {" +
+                " id = " + id +
+                ", name ='" + name + '\'' +
+                ", gradesBySubject = " + gradesBySubject +
+                '}';
+    }
 }
