73d7fb5a8f2990f4ddf797c6f7ade85b84cb3f95
Sezim <isagalievasezimka@gmail.com>
2022-02-26 21:40:06 +0600

    examination4.1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,17 @@
 package com.company;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) {
-        Set<Character> characters = new HashSet<>();
-        characters.add('1');
-        characters.add('2');
-        characters.add('3');
-        characters.add('4');
-        characters.add('4');
-        characters.add('5');
-        System.out.println(characters);
+        System.out.println("Введите число (НАПРИМЕР 1 2 3 4 4 5): ");
+        String numbers = new Scanner(System.in).nextLine();
+        Set<String> set = new HashSet<>();
+        set.addAll(Arrays.asList(numbers.split(" ")));
+        System.out.println(set);
     }
 }
